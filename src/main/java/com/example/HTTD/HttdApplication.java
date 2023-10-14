91af67a417bf646bec0358782fbaf8bf92ad73c3
phatcana116 <phatcana116@gmail.com>
2023-10-14 14:28:36 +0700

    first push

---
+++ src/main/java/com/example/HTTD/HttdApplication.java
@@ -0,0 +1,13 @@
+package com.example.HTTD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttdApplication.class, args);
+	}
+
+}
